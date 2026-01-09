ab9c897cd7983cc16f98d62c1d82913182c93d38
Razria <dsyeom931008@gmail.com>
2026-01-09 22:05:11 +0900

    ✨ feat(linkedlist): add basic linked list with push_back and debug print

--- PracticeProject03/PracticeProject03/LinkedList.h
+++ PracticeProject03/PracticeProject03/LinkedList.h
@@ -1,1 +1,172 @@
 #pragma once
+
+#include <iostream>
+#include <typeinfo>
+
+struct Node
+{
+public:
+	int Value = 0;
+	Node* Next = nullptr;
+
+public:
+	Node()
+		: Value(0), Next(nullptr)
+	{
+	}
+
+	Node(const int InValue)
+		: Value(InValue)
+	{
+	}
+};
+
+class LinkedList
+{
+private:
+	Node* Head = nullptr;
+	Node* Tail = nullptr;
+	size_t Size = 0;
+
+public:
+	LinkedList()
+	{
+		std::printf("[Constructor]\n");
+
+		Head = nullptr;
+		Tail = nullptr;
+		Size = 0;
+	}
+
+	~LinkedList()
+	{
+		for (int i = 0; i < Size; ++i)
+		{
+			if (!Head) break;
+
+			Node* old = Head;
+			Head = Head->Next;
+
+			old->Value = 0;
+			old->Next = nullptr;
+
+			delete old;
+		}
+
+		Size = 0;
+	}
+
+	LinkedList(const LinkedList&) = delete;				// copy constructor				[deprecated]
+	LinkedList& operator=(const LinkedList&) = delete;	// copy assignment operator		[deprecated]
+
+public:
+	void setSize(size_t InSize) { Size = InSize; }
+
+public:
+	size_t getSize() const { return Size; }
+
+public:
+	bool empty() const { return Size == 0; }
+
+public:
+	void push_back(int InValue)
+	{
+		Node* newNode = new Node();
+
+		newNode->Value = InValue;
+		newNode->Next = nullptr;
+
+		if (!Tail)
+		{
+			Head = newNode;
+			Tail = newNode;
+		}
+		else
+		{
+			Node* oldTail = Tail;
+			oldTail->Next = newNode;
+			Tail = newNode;
+		}
+		
+		++Size;
+	}
+
+public:
+	static void PrintLinkedListData(LinkedList* InLinkedList)
+	{
+		std::printf("[%s]\n", "PrintLinkedListData");
+		if (InLinkedList == nullptr)
+		{
+			std::printf("========================================\n");
+			std::printf("%-15s : %s\n", "LinkedList", "nullptr");
+			std::printf("========================================\n");
+			return;
+		}
+
+		std::printf("========================================\n");
+		std::printf("%-15s : %p\n", "InLinkedList", (void*)InLinkedList);
+		std::printf("%-15s : %p\n", "Head", (void*)InLinkedList->Head);
+		std::printf("%-15s : %p\n", "Tail", (void*)InLinkedList->Tail);
+		std::printf("%-15s : %zu\n", "Size", InLinkedList->Size);
+		std::printf("========================================\n");
+	}
+
+	static void PrintNodeData(const Node* InNode, size_t InIdex)
+	{
+		
+		if (InNode == nullptr)
+		{
+			std::printf("----------------------------------------\n");
+			std::printf("[%s]\n", "PrintNodeData");
+			std::printf("%-15s : %s\n", "Node", "nullptr");
+			std::printf("----------------------------------------\n");
+			return;
+		}
+
+		std::printf("----------------------------------------\n");
+		std::printf("[%s]\n", "PrintNodeData");
+		std::printf("%-15s : %zu\n", "NodeIndex", InIdex);
+		std::printf("%-15s : %p\n", "Node", (void*)InNode);
+		std::printf("%-15s : %s\n", "ValueType", typeid(int).name());
+		std::printf("%-15s : %d\n", "Value", (int)InNode->Value);
+		std::printf("%-15s : %p\n", "NextNode", (void*)InNode->Next);
+		std::printf("----------------------------------------\n");
+	}
+
+
+	static void PrintAllNodeData(LinkedList* InLinkedList)
+	{
+		if (InLinkedList == nullptr)
+		{
+			std::printf("========================================\n");
+			std::printf("[%s]\n", "PrintAllNodeData");
+			std::printf("%-15s : %s\n", "LinkedList", "nullptr");
+			std::printf("========================================\n");
+			return;
+		}
+
+		if (InLinkedList->Head == nullptr)
+		{
+			std::printf("========================================\n");
+			std::printf("[%s]\n", "PrintAllNodeData");
+			std::printf("%-15s : %p\n", "LinkedList", (const void*)InLinkedList);
+			std::printf("%-15s : %s\n", "Head", "nullptr");
+			std::printf("%-15s : %zu\n", "Size", InLinkedList->Size);
+			std::printf("========================================\n");
+			return;
+		}
+
+		const Node* printNode = InLinkedList->Head;
+		std::size_t index = 0;
+
+		std::printf("========================================\n");
+		std::printf("[%s]\n", "PrintAllNodeData");
+		while (printNode != nullptr)
+		{
+			PrintNodeData(printNode, index);
+			printNode = printNode->Next;
+			++index;
+		}
+		std::printf("========================================\n");
+	}
+};
--- PracticeProject03/PracticeProject03/main.cpp
+++ PracticeProject03/PracticeProject03/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "LinkedList.h"
+
 int main()
 {
+	LinkedList* linkedList = new LinkedList;
+	LinkedList::PrintLinkedListData(linkedList);
+
+	linkedList->push_back(1);
+	linkedList->push_back(2);
+	linkedList->push_back(3);
+
+	LinkedList::PrintLinkedListData(linkedList);
+	LinkedList::PrintAllNodeData(linkedList);
+
+	delete linkedList;
 	return 0;
 }
