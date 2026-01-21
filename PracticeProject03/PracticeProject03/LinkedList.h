a4512f6f1d080c0f37b2d09276aa5e13f6639ec5
Razria <dsyeom931008@gmail.com>
2026-01-21 13:08:42 +0900

    ✨ feat(linkedlist): implement Node data struct and default constructor

--- PracticeProject03/PracticeProject03/LinkedList.h
+++ PracticeProject03/PracticeProject03/LinkedList.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <iostream>
+#include <typeinfo>
+
+struct Node
+{
+public:
+	int Value = 0;
+	Node* Next = nullptr;
+
+public:
+	Node()
+		: Value(0), Next(nullptr)
+	{
+	}
+
+	Node(const int InValue)
+		: Value(InValue), Next(nullptr)
+	{
+	}
+};
