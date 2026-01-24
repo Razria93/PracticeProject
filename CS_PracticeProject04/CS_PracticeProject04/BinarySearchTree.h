ca9d4a81c1294ffcc9076363106ec038ac357f53
Razria <dsyeom931008@gmail.com>
2026-01-24 18:13:04 +0900

    ♻️ refactor(binary-search-tree): refactor Delete / Find / FindLargest / FindSmallest / Erase

--- CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
+++ CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
@@ -56,11 +56,11 @@ public:
 			return false; // this Node is NULL
 		}
 
-		std::printf("%-20s : Value: %-10d | Node: %p\n", "TryClear_LNode", InNode->Value, InNode);
+		std::printf("%-20s : Value: %-11d | Node: %p\n", "TryClear_LNode", InNode->Value, InNode);
 		if (Clear(InNode->Left))
 		{
 			std::printf("------------------------------\n");
-			std::printf("%-20s : ParentValue: %-10d | ParentNode: %p\n", "CompleteClear_LNode", InNode->Value, InNode);
+			std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "CompleteClear_LNode", InNode->Value, InNode);
 			std::printf("%-20s   Parent: %p |[Left : %p]| Right : %p\n", "", InNode->Parent, InNode->Left, InNode->Right);
 			std::printf("------------------------------\n");
 		}
@@ -69,11 +69,11 @@ public:
 			std::printf("%-20s\n", "InValid_LeftNode");
 		}
 
-		std::printf("%-20s : Value: %-10d | Node: %p\n", "TryClear_RNode", InNode->Value, InNode);
+		std::printf("%-20s : Value: %-11d | Node: %p\n", "TryClear_RNode", InNode->Value, InNode);
 		if (Clear(InNode->Right))
 		{
 			std::printf("------------------------------\n");
-			std::printf("%-20s : OwerValue: %-10d | OwerNode: %p\n", "CompleteClear_RNode", InNode->Value, InNode);
+			std::printf("%-20s : OwerValue: %-11d | OwerNode: %p\n", "CompleteClear_RNode", InNode->Value, InNode);
 			std::printf("%-20s   Parent: %p | Left : %p |[Right : %p]\n", "", InNode->Parent, InNode->Left, InNode->Right);
 			std::printf("------------------------------\n");
 		}
@@ -83,9 +83,9 @@ public:
 		}
 
 		std::printf("------------------------------\n");
-		std::printf("%-20s : CurValue: %-10d | CurNode: %p\n", "TryClear_CNode", InNode->Value, InNode);
+		std::printf("%-20s : CurValue: %-11d | CurNode: %p\n", "TryClear_CNode", InNode->Value, InNode);
 		Delete(InNode);
-		std::printf("%-20s : CurValue: %-10d | CurNode: %p\n", "CompleteClear_CNode", InNode->Value, InNode);
+		std::printf("%-20s : CurValue: %-11d | CurNode: %p\n", "CompleteClear_CNode", InNode->Value, InNode);
 		std::printf("%-20s   Parent: %p | Left : %p | Right : %p\n", "", InNode->Parent, InNode->Left, InNode->Right);
 		std::printf("------------------------------\n");
 
@@ -95,10 +95,45 @@ public:
 public:
 	void Delete(Node* InNode)
 	{
+		std::printf("\n[Delete Before]\n");
+		std::printf("%-20s : CurValue: %-11d | CurNode: %p\n", "Delete_BF", InNode->Value, InNode);
+		std::printf("%-20s   Parent: %p | Left : %p | Right : %p\n", "", InNode->Parent, InNode->Left, InNode->Right);
+
+		if (Node* parentNode = InNode->Parent)
+		{
+			std::printf("[Delete ParentCache Before]\n");
+			std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "Delete_BF", parentNode->Value, parentNode);
+			std::printf("%-20s   Parent: %p | Left : %p | Right : %p\n", "", parentNode->Parent, parentNode->Left, parentNode->Right);
+
+			if (InNode == parentNode->Left)
+			{
+				parentNode->Left = nullptr;
+			}
+			else if (InNode == InNode->Parent->Right)
+			{
+				parentNode->Right = nullptr;
+			}
+			std::printf("[Delete ParentCache After]\n");
+			std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "Delete_AF", parentNode->Value, parentNode);
+			std::printf("%-20s   Parent: %p | Left : %p | Right : %p\n", "", parentNode->Parent, parentNode->Left, parentNode->Right);
+			
+		}
+		else // bIsRoot?
+		{
+			if (InNode == Root)
+				Root = nullptr;
+
+			std::printf("%-20s : %s\n", "NOTE", "Root Delete");
+		}
+
 		InNode->Parent = nullptr;
 		InNode->Left = nullptr;
 		InNode->Right = nullptr;
 		InNode->Value = 0;
+
+		std::printf("[Delete After]\n");
+		std::printf("%-20s : CurValue: %-11d | CurNode: %p\n", "Delete_AF", InNode->Value, InNode);
+		std::printf("%-20s   Parent: %p | Left : %p | Right : %p\n", "", InNode->Parent, InNode->Left, InNode->Right);
 
 		delete InNode;
 	}
@@ -179,155 +214,209 @@ public:
 	} // Insert
 
 public:
-	bool Find(int InValue)
+	Node* Find(int InValue)
 	{
-		if (!Root || (Size == 0))
+		if (!Root)
 		{
-			std::printf("%-15s : %s\n", "Error", "InValid Root or Size == 0.");
-			return false;
+			return nullptr;
 		}
 
-		// Valid Root
-		Node* targetNode = Root;
-		while (targetNode != nullptr)
-		{
-			if (InValue < targetNode->Value)
-			{
-				if (!targetNode->Left)
-				{
-					std::printf("%-15s : InValue: %-5d | Node: %-5p | LastNodeValue: %-5d | InValid Direction: %-5s\n", "Find_Failed", InValue, targetNode, targetNode->Value, "Left");
-					return false;
-				}
-				else // Valid targetNode->Left
-				{
-					targetNode = targetNode->Left;
-					continue;
-				}
-			}
-			else if (InValue > targetNode->Value)
-			{
-				if (!targetNode->Right)
-				{
-					std::printf("%-15s : InValue: %-5d | Node: %-5p | LastNodeValue: %-5d | InValid Direction: %-5s\n", "Find_Failed", InValue, targetNode, targetNode->Value, "Right");
-					return false;
-				}
-				else // Valid targetNode->Left
-				{
-					targetNode = targetNode->Right;
-					continue;
-				}
-			}
-			else // Value == targetNode->Value
-			{
-				std::printf("%-15s : InValue: %-5d | Node: %-5p | LastNodeValue: %-5d\n", "Find_Complete", InValue, targetNode, targetNode->Value);
-				return true;
-			}
-		} // while
+		return Find(InValue, Root);
+	}
 
-		std::printf("%-15s : %s\n", "Error", "Undefined.");
-		return false;
+	Node* Find(int InValue, Node* InNode)
+	{
+		if (!InNode)
+		{
+			std::printf("%-20s : %s\n", "Error", "InValid InNode.");
+			return nullptr;
+		}
+
+		if (InValue == InNode->Value) return InNode;
+		if (InValue < InNode->Value) return Find(InValue, InNode->Left);
+		if (InValue > InNode->Value) return Find(InValue, InNode->Right);
+
+		std::printf("Undefined");
+		return nullptr;
 	} // Find
 
 public:
-	Node* FindLargestInSubTree(Node* InRootNode)
+	Node* FindLargest()
 	{
-		if (!InRootNode)
-		{
-			std::printf("%-15s : %s\n", "Error", "InValid InRootNode or Size == 0.");
-			return nullptr;
-		}
+		return FindLargest(Root);
+	}
 
-		// Valid Root
-		Node* targetNode = InRootNode;
-		while (targetNode != nullptr)
-		{
-			if (targetNode->Right)
-			{
-				targetNode = targetNode->Right;
-			}
-			else // InValid Right Node
-			{
-				std::printf("%-15s : Root: %-5p | Node: %-5p | LastNodeValue: %-5d\n", "Find_Largest", InRootNode, targetNode, targetNode->Value);
-				return targetNode;
-			}
-		}
-		std::printf("%-15s : %s\n", "Error", "Undefined.");
-		return nullptr;
+	Node* FindLargest(Node* InRootNode)
+	{
+		if (!InRootNode) return nullptr;
+
+		Node* largest = FindLargest(InRootNode->Right);
+
+		return largest ? largest : InRootNode;
 	}
 
 public:
-	Node* FindSmallestInSubTree(Node* InRootNode)
+	Node* FindSmallest()
 	{
-		if (!Root || (Size == 0))
-		{
-			std::printf("%-15s : %s\n", "Error", "InValid Root or Size == 0.");
-			return nullptr;
-		}
+		return FindSmallest(Root);
+	}
 
-		// Valid Root
-		Node* targetNode = Root;
-		while (targetNode != nullptr)
-		{
-			if (targetNode->Left)
-			{
-				targetNode = targetNode->Left;
-			}
-			else // InValid Right Node
-			{
-				std::printf("%-15s : Root: %-5p | Node: %-5p | LastNodeValue: %-5d\n", "Find_Smallest", InRootNode, targetNode, targetNode->Value);
-				return targetNode;
-			}
-		}
-		std::printf("%-15s : %s\n", "Error", "Undefined.");
-		return nullptr;
+	Node* FindSmallest(Node* InRootNode)
+	{
+		if (!InRootNode) return nullptr;
+
+		Node* smallest = FindSmallest(InRootNode->Left);
+
+		return smallest ? smallest : InRootNode;
 	}
 
 public:
 	bool Erase(int InValue)
 	{
-		if (!Root || (Size == 0))
+		return Erase(InValue, Root);
+	}
+
+	bool Erase(int InValue, Node* InNode)
+	{
+		if (!InNode)
 		{
-			std::printf("%-15s : %s\n", "Error", "InValid Root or Size == 0.");
+			std::printf("%-20s : %s\n", "Error", "InValid InNode.");
 			return false;
 		}
 
-		// Valid Root
-		Node* targetNode = Root;
-		while (targetNode != nullptr)
+		if (InValue == InNode->Value)
 		{
-			if (InValue < targetNode->Value)
-			{
-				if (!targetNode->Left)
-				{
-					std::printf("%-15s : InValue: %-5d | Node: %-5p | LastNodeValue: %-5d | InValid Direction: %-5s\n", "Erase_Failed", InValue, targetNode, targetNode->Value, "Left");
-					return false;
-				}
-				else // Valid targetNode->Left
-				{
-					targetNode = targetNode->Left;
-					continue;
-				}
-			}
-			else if (InValue > targetNode->Value)
-			{
-				if (!targetNode->Right)
-				{
-					std::printf("%-15s : InValue: %-5d | Node: %-5p | LastNodeValue: %-5d | InValid Direction: %-5s\n", "Erase_Failed", InValue, targetNode, targetNode->Value, "Right");
-					return false;
-				}
-				else // Valid targetNode->Left
-				{
-					targetNode = targetNode->Right;
-					continue;
-				}
-			}
-			else // Value == targetNode->Value
-			{
-				// TODO
-			}
-		} // while
+			Node* eraseNode = InNode;
+			Node* switchNode = nullptr;
 
-		std::printf("%-15s : %s\n", "Error", "Undefined.");
+			Node* largest = FindLargest(InNode->Left);
+			Node* smallest = FindSmallest(InNode->Right);
+
+			if (!largest && !smallest) return false;
+			
+			if (!largest && smallest)
+				switchNode = smallest;
+			
+			if (largest && !smallest)
+				switchNode = largest;
+
+			if (largest && smallest)
+			{
+				std::printf("%-20s : InNodeValue: %-11d | LargestValue Of LeftNode: %-11d | SmallestValue Of RghtNode: %-11d\n", "ValueCompation", InNode->Value, largest->Value, smallest->Value);
+
+				if ((largest->Value - InNode->Value) >= (InNode->Value - smallest->Value))
+				{
+					switchNode = smallest;
+				}
+				else
+				{
+					switchNode = largest;
+				}
+			}
+
+			Node* oldParentNode = switchNode->Parent ? switchNode->Parent : nullptr;
+			Node* newParentNode = eraseNode->Parent ? eraseNode->Parent : nullptr;
+
+			std::printf("\n[Erase Before]\n");
+			if (oldParentNode)
+			{
+				std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "OldParentNode", oldParentNode->Value, oldParentNode);
+				std::printf("%-20s   Parent: %p | Left : %p |[Right : %p]\n", "", oldParentNode->Parent, oldParentNode->Left, oldParentNode->Right);
+			}
+			else
+			{
+				std::printf("OldParentNode InValid");
+			}
+
+			if (newParentNode)
+			{
+				std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "NewParentNode", newParentNode->Value, newParentNode);
+				std::printf("%-20s   Parent: %p |[Left : %p]| Right : %p\n", "", newParentNode->Parent, newParentNode->Left, newParentNode->Right);
+			}
+			else
+			{
+				std::printf("NewParentNode InValid");
+			}
+
+
+			std::printf("%-20s : SwitchValue: %-11d | SwitchNode: %p\n", "SwicthNode", switchNode->Value, switchNode);
+			std::printf("%-20s  [Parent: %p]| Left : %p | Right : %p\n", "", switchNode->Parent, switchNode->Left, switchNode->Right);
+			std::printf("%-20s : %zd\n", "Size", Size);
+
+			bool bRight = false;
+			bool bLeft = false;
+
+			if (newParentNode)
+			{
+				if (newParentNode->Right == eraseNode)
+					bRight = true;
+				else if (newParentNode->Left == eraseNode)
+					bLeft = true;
+			}
+
+			Delete(eraseNode);
+
+			if (oldParentNode)
+			{
+				if (oldParentNode->Right == switchNode)
+					oldParentNode->Right = nullptr;
+				else if (oldParentNode->Left == switchNode)
+					oldParentNode->Left = nullptr;
+			}
+
+			if (newParentNode)
+			{
+				if (bRight)
+				{
+					newParentNode->Right = switchNode;
+					switchNode->Parent = newParentNode;
+				}
+				else if (bLeft)
+				{
+					newParentNode->Left = switchNode;
+					switchNode->Parent = newParentNode;
+				}
+			}
+			else
+			{
+				switchNode->Parent = nullptr;
+			}
+			
+			--Size;
+
+			std::printf("\n[Erase After]\n");
+			if (oldParentNode)
+			{
+				std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "OldParentNode", oldParentNode->Value, oldParentNode);
+				std::printf("%-20s   Parent: %p | Left : %p |[Right : %p]\n", "", oldParentNode->Parent, oldParentNode->Left, oldParentNode->Right);
+			}
+			else
+			{
+				std::printf("OldParentNode InValid");
+			}
+
+			if (newParentNode)
+			{
+				std::printf("%-20s : ParentValue: %-11d | ParentNode: %p\n", "NewParentNode", newParentNode->Value, newParentNode);
+				std::printf("%-20s   Parent: %p |[Left : %p]| Right : %p\n", "", newParentNode->Parent, newParentNode->Left, newParentNode->Right);
+			}
+			else
+			{
+				std::printf("NewParentNode InValid");
+			}
+
+			std::printf("%-20s : SwitchValue: %-11d | SwitchNode: %p\n", "SwicthNode", switchNode->Value, switchNode);
+			std::printf("%-20s  [Parent: %p]| Left : %p | Right : %p\n", "", switchNode->Parent, switchNode->Left, switchNode->Right);
+			std::printf("%-20s : %zd\n", "Size", Size);
+			
+
+			return true;
+		}
+
+		if (InValue < InNode->Value) return Erase(InValue, InNode->Left);
+		if (InValue > InNode->Value) return Erase(InValue, InNode->Right);
+
+		std::printf("Undefined");
 		return false;
-	}
+	} // Erase
 };
--- CS_PracticeProject04/CS_PracticeProject04/main.cpp
+++ CS_PracticeProject04/CS_PracticeProject04/main.cpp
@@ -14,9 +14,34 @@ int main()
 	bst.Insert(200);
 	bst.Insert(500);
 	bst.Insert(400);
+	bst.Insert(25);
+	bst.Insert(75);
 
 	std::printf("\n[Find]\n");
-	bst.Find(300);
+	Node* find_before = bst.Find(100);
+	std::printf("%-20s : Node: %-5p | InValue: %-5d\n", "Find_Result", find_before, find_before ? find_before->Value : -1);
+
+	Node* largest = bst.FindLargest();
+	std::printf("%-20s : Node: %-5p | InValue: %-5d\n", "FindLargest", largest, largest ? largest->Value : -1);
+
+	Node* smallest = bst.FindSmallest();
+	std::printf("%-20s : Node: %-5p | InValue: %-5d\n", "FindSmallest", smallest, largest ? smallest->Value : -1);
+
+	std::printf("\n[Erase_0]\n");
+	bool result_0 = bst.Erase(50);
+	std::printf("\n%-20s : %s\n", "[Erase_Result]", result_0 ? "Complete" : "Fail");
+
+	std::printf("\n[Erase_1]\n");
+	bool result_1 = bst.Erase(300);
+	std::printf("\n%-20s : %s\n", "[Erase_Result]", result_1 ? "Complete" : "Fail");
+	
+	std::printf("\n[Find_0]\n");
+	Node* find_0 = bst.Find(50);
+	std::printf("%-20s : Node: %-5p | InValue: %-5d\n", "[Find_Result]", find_0, find_0 ? find_0->Value : -1);
+
+	std::printf("\n[Find_1]\n");
+	Node* find_1 = bst.Find(300);
+	std::printf("%-20s : Node: %-5p | InValue: %-5d\n", "[Find_Result]", find_1, find_1 ? find_1->Value : -1);
 
 	std::printf("\n[Clear]\n");
 	if (bst.Clear())
